27ddd2353c9939c34a5a12f1e3a9d352a574842d
Mazen <mmeezzoo26@me.com>
2016-02-06 17:22:32 +0100

    Unit test done, green is good!

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -1,31 +1,45 @@
 describe("FizzBuzz - Unit tests", function() {
-  var slot;
+  //var slot;
   var game;
 
   beforeEach(function() {
-    slot = new FizzBuzz();
-    game = new Game({number: 6});
-    game_2 = new Game({number: 10});
-    game_3 = new Game({number: 30});
-    game_4 = new Game({number: 26});
+    game = new FizzBuzz();
+    divisable_check = (9,3)
+    try_1 = (12)
+    try_2 = (20)
+    try_3 = (45)
+    try_4 = (26)
   });
 
   it("check if 9 is divisable by 3 ", function() {
-    expect(slot._hasZeroReminder(9,3)).toEqual(true);
+    expect(game._hasZeroReminder(10,2)).toEqual(true);
   });
 
   it("check if number is divisable by 3", function() {
-    //slot._hasZeroReminder(game, 3);
-    expect(game.divisibleByThree).toEqual(2);
+    expect(game.divisibleByThree(try_1)).toEqual(true);
   });
 
   it("check if number is divisable by 5", function() {
-    slot._hasZeroReminder(game, 3);
-    expect(game.divisibleByThree).toEqual(2);
+    expect(game.divisibleByFive(try_2)).toEqual(true);
   });
 
   it("check if number is divisable by 15", function() {
-    slot._hasZeroReminder(game, 3);
-    expect(game.divisibleByThree).toEqual(2);
+    expect(game.divisibleByFifteen(try_3)).toEqual(true);
+  });
+
+  it("return fizz if number is divisable by 3", function() {
+    expect(game.fizz_buzz(try_1)).toEqual("Fizz");
+  });
+
+  it("return buzz if number is divisable by 5", function() {
+    expect(game.fizz_buzz(try_2)).toEqual("Buzz");
+  });
+
+  it("return fizzbuzz if number is divisable by 3 & 5 ", function() {
+    expect(game.fizz_buzz(try_3)).toEqual("FizzBuzz");
+  });
+
+  it("return same number if number is not divisable by 3 or 5", function() {
+    expect(game.fizz_buzz(try_4)).toEqual(26);
   });
 });
--- src/js/game.js
+++
@@ -1,8 +0,0 @@
-function Game(attr) {
-  this.number = attr.number;
-};
-
-Game.prototype.cal_fizz_buzz = function() {
-  calculator = new FizzBuzz();
-  calculator.fizz_buzz(this);
-};
